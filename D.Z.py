f10e4b67a521406efb5762d9fb0cdb4daf7a2af0
Bosco <andrijpeh489@gmail.com>
2024-03-02 18:58:44 +0200

    Додаємо функцію rjadok(root):

--- D.Z.py
+++ D.Z.py
@@ -52,7 +52,27 @@ def knopku(btn, entry_field):
     current_formula = entry_field.get()
     operatcii(btn, current_formula, entry_field)
 
-
+def rjadok(root):
+    entry_field = Entry(root, font=("Times New Roman", 21, "bold"), bg="white", justify=RIGHT)
+    entry_field.place(x=10, y=10, width=465, height=40)
+    entry_field.insert(0, "0")
+    entry_field.bind("<KeyPress>", check_input)
+    btns = ["C", "DEL", "*", "=",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "(", "0", ")", "X^2",
+            "sin", "cos", "tan",
+            "Вихід"]
+    x = 10
+    y = 60
+    for btn in btns:
+        com = lambda x=btn: knopku(x, entry_field)
+        Button(text=btn, bg="white", font=("Times New Roman", 15), command=com).place(x=x, y=y, width=115, height=79)
+        x += 117
+        if x > 400:
+            x = 10
+            y += 81
 if __name__ == '__main__':
     root = Tk()
     root["bg"] = "black"
